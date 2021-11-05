c907a0e7f80c3b4fb9a83a30ebc04ab7bae2191d
pulkit juneja <pulkit.16296@gmail.com>
2021-11-05 12:34:13 -0400

    initial commit

---
+++ Source/ARTAudioAnalyzer/ARTAudioAnalyzer.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ARTAudioAnalyzer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ARTAudioAnalyzer, "ARTAudioAnalyzer" );
